4fc471bfe297705200a244103138c8d884ab2777
LascanoAldahir <aldahirlascano@gmail.com>
2024-05-20 15:34:01 -0500

    terminado

--- Hilocliente.java
+++ Hilocliente.java
@@ -1,52 +1,88 @@
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class hiloCliente extends Thread {
-    DatagramSocket socket_cliente;
-    List<Question> questions;
-    InetAddress address;
-    int port;
+class HiloCliente extends Thread {
+    //crear variable para el puerto
+    private int puerto;
 
-    public hiloCliente(DatagramSocket socket_cliente, List<Question> questions, InetAddress address, int port) {
-        this.socket_cliente = socket_cliente;
-        this.questions = questions;
-        this.address = address;
-        this.port = port;
+    //arreglo para almacenar las preguntas y la respuesta
+    private Map<String, String> preguntas;
+
+    public HiloCliente(int puerto) {
+        this.puerto = puerto;
+        this.preguntas = new HashMap<>();
+        this.preguntas.put("Iniciales de escuela politecnica", "epn");
+        this.preguntas.put("tercer planeta del sistema solar", "tierra");
+        this.preguntas.put("amigo de doraemon", "nobita");
+        this.preguntas.put("capital de ecuador", "quito");
+        this.preguntas.put("Religión de jesus", "cristiana");
     }
 
+    @Override
     public void run() {
         try {
-            for (Question question : questions) {
-                sendPacket(socket_cliente, address, port, question.getQuestion());
-                DatagramPacket packet = receivePacket(socket_cliente, address, port);
-                String respuesta = new String(packet.getData(), 0, packet.getLength());
+            DatagramSocket socket = new DatagramSocket(puerto);
+            //mesnaje para ver la conexion
+            System.out.println("Esperando la conexión del cliente...");
 
-                if (respuesta.equals(question.getCorrectAnswer())) {
-                    sendPacket(socket_cliente, address, port, "Correcto");
-                } else {
-                    sendPacket(socket_cliente, address, port, "Incorrecto. La respuesta correcta es: " + question.getCorrectAnswer());
+            while (true) {
+                byte[] bufferEntrada = new byte[1024];
+                DatagramPacket paqueteEntrada = new DatagramPacket(bufferEntrada, bufferEntrada.length);
+                socket.receive(paqueteEntrada);
+
+                //obtener la direccion del cliente
+                InetAddress direccionCliente = paqueteEntrada.getAddress();
+                int puertoCliente = paqueteEntrada.getPort();
+                
+                System.out.println("Conexión establecida con el cliente en la dirección IP: " + direccionCliente.getHostAddress());
+
+                int puntajeTotal = 0;
+                //evaluar las preguntas
+                for (Map.Entry<String, String> entry : preguntas.entrySet()) {
+                    String pregunta = entry.getKey();
+                    String respuestaCorrecta = entry.getValue();
+
+                    // Enviar pregunta al cliente
+                    byte[] bufferPregunta = pregunta.getBytes();
+                    DatagramPacket paquetePregunta = new DatagramPacket(bufferPregunta, bufferPregunta.length, direccionCliente, puertoCliente);
+                    socket.send(paquetePregunta);
+
+                    // Recibir respuesta del cliente
+                    byte[] bufferRespuesta = new byte[1024];
+                    DatagramPacket paqueteRespuesta = new DatagramPacket(bufferRespuesta, bufferRespuesta.length);
+                    socket.receive(paqueteRespuesta);
+                    String respuesta = new String(paqueteRespuesta.getData(), 0, paqueteRespuesta.getLength());
+
+                    // Verificar respuesta
+                    String mensajeSalida;
+                    if (respuesta.trim().equalsIgnoreCase(respuestaCorrecta)) {
+                        mensajeSalida = "Respuesta correcta";
+                        puntajeTotal += 4;
+                    } else {
+                        mensajeSalida = "Respuesta incorrecta. La respuesta correcta era: " + respuestaCorrecta;
+                    }
+
+                    // Enviar confirmación al cliente
+                    byte[] bufferSalida = mensajeSalida.getBytes();
+                    DatagramPacket paqueteSalida = new DatagramPacket(bufferSalida, bufferSalida.length, direccionCliente, puertoCliente);
+                    socket.send(paqueteSalida);
                 }
-            }
 
-            sendPacket(socket_cliente, address, port, "Puntaje final: " + (questions.size() * 4));
-        } catch (IOException e) {
+                // Enviar puntaje final al cliente
+                String puntajeFinal = "Puntaje final: " + puntajeTotal;
+                byte[] bufferPuntaje = puntajeFinal.getBytes();
+                DatagramPacket paquetePuntaje = new DatagramPacket(bufferPuntaje, bufferPuntaje.length, direccionCliente, puertoCliente);
+                socket.send(paquetePuntaje);
+
+                System.out.println("Puntaje final enviado al cliente: " + puntajeTotal);
+
+                socket.close();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private static DatagramPacket receivePacket(DatagramSocket socket, InetAddress address, int port) throws IOException {
-        byte[] buffer = new byte[1024];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
-        return packet;
-    }
-
-    private static void sendPacket(DatagramSocket socket, InetAddress address, int port, String message) throws IOException {
-        byte[] buffer = message.getBytes();
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
-        socket.send(packet);
     }
 }
